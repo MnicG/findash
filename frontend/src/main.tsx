4056cd8f5994a8232bf78ac86d9325d00388c24d
MnicG <goulartn64@gmail.com>
2026-05-26 20:20:59 -0300

    feat: restructure component hierarchy to wrap App with AuthProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './contexts/AuthContext'
 import { SettingsProvider } from './contexts/SettingsContext'
 import './index.css'
 import App from './App.tsx'
@@ -10,9 +11,11 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
